d6255f867597211340ca5da688779bb6a2d09c8d
Owen Cooke <ocooke@ualberta.ca>
2024-01-15 00:28:40 -0700

    changing modal to allow for reps/sets

--- src/views/practitioner/components/ExerciseListModal.jsx
+++ src/views/practitioner/components/ExerciseListModal.jsx
@@ -1,56 +1,89 @@
 import { useState } from "react";
 
-const ExerciseListModal = ({ exercises, onAddExercises, onClose }) => {
-  const [selectedExercises, setSelectedExercises] = useState([]);
+const id = "exercise_list_modal";
 
-  const handleSelectExercise = (exerciseId) => {
-    setSelectedExercises((prev) => {
-      if (prev.includes(exerciseId)) {
-        return prev.filter((id) => id !== exerciseId);
-      } else {
-        return [...prev, exerciseId];
-      }
-    });
-  };
+const ExerciseListModal = ({ exercises, onAddExercises }) => {
+  const [formState, setFormState] = useState({
+    title: "",
+    sets: "",
+    reps: "",
+    additionalNotes: "",
+  });
 
   const handleClose = () => {
-    document.getElementById("exercise_list_modal").close();
-    onClose();
+    document.getElementById(id).close();
   };
 
   const handleSubmit = () => {
-    onAddExercises(selectedExercises);
+    onAddExercises(formState);
     handleClose();
   };
 
+  const onFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
-    <dialog id="exercise_list_modal" className="modal">
+    <dialog id={id} className="modal">
       <div className="modal-box">
-        <form method="dialog">
-          <button
+        <button
           type="button"
           className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
           onClick={handleClose}
         >
           ✕
         </button>
-        </form>
-        <h3 className="font-bold text-lg">Select Exercises</h3>
-        <div className="py-4">
-          {exercises.map((exercise) => (
-            <div key={exercise.id} className="flex items-center">
-              <input
-                type="checkbox"
-                checked={selectedExercises.includes(exercise.id)}
-                onChange={() => handleSelectExercise(exercise.id)}
-                className="checkbox checkbox-primary mt-1 border-gray-500 active:bg-gray-500 active:text-gray-500 focus:ring-gray-500 dark:focus:ring-gray-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 text-gray-600"
-              />
-              <span className="ml-2">{exercise.title}</span>
-            </div>
-          ))}
+        <h3 className="font-bold text-lg">Add Exercise</h3>
+        <div className="grid grid-cols-2 gap-4 p-4 items-center">
+          <div className="font-bold">Name</div>
+          <select
+            name="title"
+            className="select select-bordered w-full max-w-xs"
+            onChange={onFormChange}
+            value={formState.title}
+          >
+            <option disabled selected>
+              Select Exercise
+            </option>
+            {exercises.map((exercise) => (
+              <option key={exercise.id}>{exercise.title}</option>
+            ))}
+          </select>
+          <div className="font-bold">Sets</div>
+          <input
+            name="sets"
+            type={"number"}
+            className="w-full px-3 py-2 border rounded-md"
+            onChange={onFormChange}
+            value={formState.sets}
+          />
+          <div className="font-bold">Reps</div>
+          <input
+            name="reps"
+            type={"number"}
+            className="w-full px-3 py-2 border rounded-md"
+            onChange={onFormChange}
+            value={formState.reps}
+          />
+          <div className="col-span-2">
+            <div className="font-bold">Additional Notes</div>
+            <textarea
+              name="additionalNotes"
+              className="w-full px-3 py-2 border rounded-md"
+              onChange={onFormChange}
+              value={formState.additionalNotes}
+            ></textarea>
+          </div>
         </div>
         <div className="modal-action">
-          <button onClick={handleSubmit} className="btn bg-gray-800 text-white hover:bg-gray-600">
+          <button
+            onClick={handleSubmit}
+            className="btn bg-gray-800 text-white hover:bg-gray-600"
+          >
             Add to Routine
           </button>
         </div>
--- src/views/practitioner/components/PatientDetails.jsx
+++ src/views/practitioner/components/PatientDetails.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import { db, getCurrentUser } from "../../../../firebaseConfig";
-import { Edit } from "lucide-react";
+import { Edit, Save, X, Plus } from "lucide-react";
 import ExerciseListModal from "./ExerciseListModal";
 
 const PatientDetails = ({ patientID }) => {
   const [patient, setPatient] = useState(null);
   const [exercises, setExercises] = useState([]);
   const [patientExercises, setPatientExercises] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [convos, setConvos] = useState([]); 
+  const [convos, setConvos] = useState([]);
+  const [editing, setEditing] = useState(false);
 
   useEffect(() => {
     const fetchPatientDetails = async () => {
@@ -67,35 +67,39 @@ const PatientDetails = ({ patientID }) => {
     fetchPatientDetails();
   }, [patientID]);
 
-  const handleAddExercises = async (selectedExercises) => {
-    const currentUser = await getCurrentUser();
-    const patientRef = db.collection("practitioners").doc(currentUser.uid).collection("patients").doc(patientID);
-    await patientRef.update({
-      exerciseRoutine: selectedExercises
-    });
+  // const handleAddExercises = async (selectedExercises) => {
+  //   const currentUser = await getCurrentUser();
+  //   const patientRef = db
+  //     .collection("practitioners")
+  //     .doc(currentUser.uid)
+  //     .collection("patients")
+  //     .doc(patientID);
+  //   await patientRef.update({
+  //     exerciseRoutine: selectedExercises,
+  //   });
 
-    setPatientExercises(selectedExercises);
-    setShowModal(false);
-  };
+  //   setPatientExercises(selectedExercises);
+  // };
 
-  const toggleModal = () => {
-    const modal = document.getElementById('exercise_list_modal');
-    if (modal) {
-      showModal ? modal.close() : modal.showModal();
-      setShowModal(!showModal);
-    } else {
-      console.error("Modal element not found");
-    }
+  const handleAddExercise = (newExercise) => {
+    const exercise = exercises.find((e) => e.title === newExercise.title);
+    setPatientExercises((prev) => [...prev, exercise.id]);
   };
 
   const formatDate = (date) => {
-    if (!date) return '';
+    if (!date) return "";
     const day = date.getDate();
     const month = date.getMonth() + 1; // Months are zero-based
     const year = date.getFullYear();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    return `${day}/${month}/${year} ${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+    return `${day}/${month}/${year} ${hours}:${
+      minutes < 10 ? "0" : ""
+    }${minutes}`;
+  };
+
+  const handleEditing = () => {
+    setEditing((prev) => !prev);
   };
 
   return (
@@ -109,51 +113,105 @@ const PatientDetails = ({ patientID }) => {
             <p>{patient.email}</p>
             <p>Last Login: {patient.lastLogin || "Never"}</p>
           </div>
-          <div className="flex justify-between w-full text-xl font-bold mt-10">
+          <div className="flex justify-between items-center w-full text-xl font-bold mt-10">
             <p>Exercise Routine</p>
-            <button
-              className={"btn bg-dark-teal text-white"}
-              onClick={toggleModal}
-            >
-              <Edit />
-              Add Exercises
-            </button>
+            {editing ? (
+              <div>
+                <button
+                  className="btn bg-dark-teal text-white"
+                  onClick={() => handleEditing()}
+                >
+                  <Save />
+                </button>
+                <button className="btn" onClick={() => handleEditing()}>
+                  <X />
+                </button>
+              </div>
+            ) : (
+              <button
+                className={"btn bg-dark-teal text-white"}
+                onClick={() => handleEditing()}
+              >
+                <Edit />
+              </button>
+            )}
           </div>
           {/* Display exercises in the patient's routine as cards */}
           <div className="bg-gray-50 rounded-lg mt-4">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 p-5">
-              {patientExercises.length === 0 ? (
-                <p className="text-gray-600">No exercises assigned in routine.</p>
+              {patientExercises.length === 0 && !editing ? (
+                <p className="text-gray-600">No exercises assigned 🥱</p>
               ) : (
-                patientExercises.map(exerciseId => {
-                  const exercise = exercises.find(e => e.id === exerciseId);
-                  return exercise && (
-                    <div key={exercise.id} className="bg-white rounded-lg shadow-md p-4 flex flex-col">
-                      <img src={exercise.image} alt={exercise.title} className="w-80 h-32 sm:h-48 object-fit rounded-t-lg" />
-                      <h3 className="mt-2 font-bold text-lg">{exercise.title}</h3>
-                      <p className="text-gray-600 text-sm mt-1">{exercise.description}</p>
-                    </div>
-                  );
-                })
+                <>
+                  {" "}
+                  {patientExercises.map((exerciseId) => {
+                    const exercise = exercises.find((e) => e.id === exerciseId);
+                    return (
+                      exercise && (
+                        <div
+                          key={exercise.id}
+                          className="bg-white rounded-lg shadow-md p-4 flex flex-col"
+                        >
+                          <img
+                            src={exercise.image}
+                            alt={exercise.title}
+                            className="w-80 h-32 sm:h-48 object-fit rounded-t-lg"
+                          />
+                          <h3 className="mt-2 font-bold text-lg">
+                            {exercise.title}
+                          </h3>
+                          <div className="grid grid-cols-2 gap-4 p-2">
+                            <div className="font-bold">Sets</div>
+                            <div>{exercise.sets}</div>
+                            <div className="font-bold">Reps</div>
+                            <div>{exercise.reps}</div>
+                            <div className="font-bold">Notes</div>
+                            <div>{exercise.notes}</div>
+                          </div>
+                        </div>
+                      )
+                    );
+                  })}
+                  {editing && (
+                    <button
+                      className="btn bg-dark-teal text-white h-full"
+                      onClick={() =>
+                        document
+                          .getElementById("exercise_list_modal")
+                          .showModal()
+                      }
+                    >
+                      <Plus size="64" />
+                    </button>
+                  )}
+                </>
               )}
             </div>
           </div>
           <div>
-          { convos && convos.length !== 0 ? (
-            <div>
-          <p className="flex justify-between w-full text-xl font-bold mt-10">Conversation Summaries</p>
-          <div className="flex justify-between w-full text-gray-600 text-xl font-bold bg-gray-50 rounded-lg pl-2 pr-2">
-            <p>Date</p>
-            <p>Summary</p>
+            {convos && convos.length !== 0 ? (
+              <div>
+                <p className="flex justify-between w-full text-xl font-bold mt-10">
+                  Conversation Summaries
+                </p>
+                <div className="flex justify-between w-full text-gray-600 text-xl font-bold bg-gray-50 rounded-lg pl-2 pr-2">
+                  <p>Date</p>
+                  <p>Summary</p>
+                </div>
+                {convos.map((convo, index) => (
+                  <div
+                    key={index}
+                    className="flex justify-between w-full text-gray-600 text-xl bg-gray-50 rounded-lg pl-2 pr-2"
+                  >
+                    <p>{formatDate(convo.date)}</p>
+                    <p>{convo.summary}</p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div></div>
+            )}
           </div>
-          {convos.map((convo, index) => (
-            <div key={index} className="flex justify-between w-full text-gray-600 text-xl bg-gray-50 rounded-lg pl-2 pr-2">
-              <p>{formatDate(convo.date)}</p>
-              <p>{convo.summary}</p>
-            </div>
-          ))}
-          </div>) : (<div></div>) }
-            </div>
         </div>
       ) : (
         <p>Loading patient details...</p>
@@ -161,8 +219,7 @@ const PatientDetails = ({ patientID }) => {
 
       <ExerciseListModal
         exercises={exercises}
-        onAddExercises={handleAddExercises}
-        onClose={() => setShowModal(false)}
+        onAddExercises={handleAddExercise}
       />
     </div>
   );
